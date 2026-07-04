359be35a580b88e58d599391b658856b005c247d
Vitalii Boiko <vitaliyboyko1@gmail.com>
2026-07-04 19:05:05 +0300

    add: new ver

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
 
 require('./server');
 const fs = require("fs");
@@ -13,6 +14,28 @@ function createWindow() {
     win.loadURL('http://localhost:3000');
 }
 
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        title: 'Оновлення',
+        message: 'Доступна нова версія. Завантажую...'
+    });
+});
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        title: 'Оновлення готове',
+        message: 'Оновлення завантажено. Натисни OK щоб перезапустити.',
+        buttons: ['OK']
+    }).then(() => {
+        autoUpdater.quitAndInstall();
+    });
+});
+
+autoUpdater.on('error', (err) => {
+    console.error('AutoUpdater error:', err);
+});
+
 app.whenReady().then(() => {
     setTimeout(createWindow, 1000);
+    autoUpdater.checkForUpdatesAndNotify();
 });
